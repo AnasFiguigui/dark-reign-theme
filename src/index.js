2c1cb3810b2c9c031600704b99401bf1ac16b912
AnasFiguigui <anas.production.af@gmail.com>
2026-02-13 11:42:48 +0100

    Chnaged the default theme

--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ const fs = require("fs").promises;
 const getTheme = require("./theme");
 const getClassicTheme = require("./classic/theme");
 
-const lightDefaultTheme = getTheme({
-  theme: "light",
-  name: "GitHub Light Default",
-});
-
-const lightHighContrastTheme = getTheme({
-  theme: "light_high_contrast",
-  name: "GitHub Light High Contrast"
-})
-
-const lightColorblindTheme = getTheme({
-  theme: "light_colorblind",
-  name: "GitHub Light Colorblind",
+// Anas theme
+const darkAnasTheme = getTheme({
+  theme: "dark",
+  name: "GitHub Dark Anas",
 });
 
 const darkDefaultTheme = getTheme({
@@ -39,11 +30,6 @@ const darkDimmedTheme = getTheme({
 
 // Classic
 
-const lightTheme = getClassicTheme({
-  style: "light",
-  name: "GitHub Light",
-});
-
 const darkTheme = getClassicTheme({
   style: "dark",
   name: "GitHub Dark",
@@ -53,14 +39,11 @@ const darkTheme = getClassicTheme({
 
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
-    fs.writeFile("./themes/light-default.json", JSON.stringify(lightDefaultTheme, null, 2)),
-    fs.writeFile("./themes/light-high-contrast.json", JSON.stringify(lightHighContrastTheme, null, 2)),
-    fs.writeFile("./themes/light-colorblind.json", JSON.stringify(lightColorblindTheme, null, 2)),
+    // fs.writeFile("./themes/dark-anas.json", JSON.stringify(darkAnasTheme, null, 2)),
     fs.writeFile("./themes/dark-default.json", JSON.stringify(darkDefaultTheme, null, 2)),
     fs.writeFile("./themes/dark-high-contrast.json", JSON.stringify(darkHighContrastTheme, null, 2)),
     fs.writeFile("./themes/dark-colorblind.json", JSON.stringify(darkColorblindTheme, null, 2)),
     fs.writeFile("./themes/dark-dimmed.json", JSON.stringify(darkDimmedTheme, null, 2)),
-    fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2)),
     fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2)),
   ]))
   .catch(() => process.exit(1))
